f92e0f2dc60733a8b31df4e869b3977eb6138e9c
k.zolfaghari <k.zolfaghari@gmail.com>
2025-12-03 22:46:01 +0000

    updating nyc

--- js/grayscale.js
+++ js/grayscale.js
@@ -7,9 +7,9 @@
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+        $(".fixed-top").addClass("top-nav-collapse");
     } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $(".fixed-top").removeClass("top-nav-collapse");
     }
 });
 
